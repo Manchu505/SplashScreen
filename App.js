34cb861be82b9c7fbc221d2eebf0d071364e524e
Manchu505 <19881829+Manchu505@users.noreply.github.com>
2023-12-11 18:46:55 -0600

    Final

--- App.js
+++ App.js
@@ -1,87 +1,24 @@
 import React, {useState, useEffect} from 'react';
-import { StatusBar } from 'expo-status-bar';
-import {
-  SafeAreaView,
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  Button,
-} from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
-import QRCode from 'react-native-qrcode-svg';
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import User from './screens/UserScreen';
-import Home from './screens/HomeScreen';
-import Contact from './screens/ContactCard';
-import Settings from './screens/SettingScreen';
+import 'react-native-gesture-handler';
+import AnyQRCode from './screens/AnyQRCode';
 import QRCodeGenerator from './screens/ContactCard';
-import Sound from './screens/ObligatorySound';
+import Camera from './screens/Camera';
+import QRCodeDisplay from './QRCodeDisplay';
 
 
 //The splash screen is shown for 5 seconds before hiding automatically.
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 5000);
-
-
-const styles = StyleSheet.create({ 
-    container: { 
-        flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        backgroundColor: '#eee', 
-    }, 
-    wrapper: { 
-        maxWidth: 300, 
-        backgroundColor: '#fff', 
-        borderRadius: 7, 
-        padding: 20, 
-        shadowColor: 'rgba(0, 0, 0, 0.1)', 
-        shadowOffset: { width: 0, height: 10 }, 
-        shadowOpacity: 1, 
-        shadowRadius: 30, 
-    }, 
-    title: { 
-        fontSize: 21, 
-        fontWeight: '500', 
-        marginBottom: 10, 
-    }, 
-    description: { 
-        color: '#575757', 
-        fontSize: 16, 
-        marginBottom: 20, 
-    }, 
-    input: { 
-        fontSize: 18, 
-        padding: 17, 
-        borderWidth: 1, 
-        borderColor: '#999', 
-        borderRadius: 5, 
-        marginBottom: 20, 
-    }, 
-    button: { 
-        backgroundColor: '#3498DB', 
-        borderRadius: 5, 
-        padding: 15, 
-        alignItems: 'center', 
-    }, 
-    buttonText: { 
-        color: '#fff', 
-        fontSize: 18, 
-    }, 
-    qrCode: { 
-        marginTop: 20, 
-        alignItems: 'center', 
-    }, 
-});
  
 //A navigation component that creates a bottom tab navigation
 const Tab = createBottomTabNavigator();
 
-function MyTabs() {
+export function MyTabs() {
 	return (
 	  <Tab.Navigator
 		initialRouteName="Home"
@@ -89,29 +26,9 @@ function MyTabs() {
 		  tabBarActiveTintColor: '#e91e63',
 		}}
 	  >
-		<Tab.Screen
-		  name="Get Ready to Generate"
-		  component={Home}
-		  options={{
-			tabBarLabel: 'Home',
-			tabBarIcon: ({ color, size }) => (
-			  <MaterialCommunityIcons name="home-circle" color={color} size={size} />
-			),
-		  }}
-		/>
-		<Tab.Screen
-		  name="Let's Make Some Noise"
-		  component={Sound}
-		  options={{
-			tabBarLabel: 'Sound',
-			tabBarIcon: ({ color, size }) => (
-			  <MaterialCommunityIcons name="hexagon-slice-5" color={color} size={size} />
-			),
-		  }}
-		/>
-		<Tab.Screen
+    <Tab.Screen
 		  name="Digital vCard"
-		  component={QRCodeGenerator}
+		  component={HomeStack}
 		  options={{
 			tabBarLabel: 'vCard',
 			tabBarIcon: ({ color, size }) => (
@@ -119,14 +36,49 @@ function MyTabs() {
 			),
 		  }}
 		/>
+		<Tab.Screen
+		  name="Let's Scan Some Codes!"
+		  component={Camera}
+		  options={{
+			tabBarLabel: 'Camera',
+			tabBarIcon: ({ color, size }) => (
+			  <MaterialCommunityIcons name="hexagon-slice-5" color={color} size={size} />
+			),
+		  }}
+		/>
+    <Tab.Screen
+		  name="Get Ready to Generate"
+		  component={AnyQRCode}
+		  options={{
+			tabBarLabel: 'QR Code',
+			tabBarIcon: ({ color, size }) => (
+			  <MaterialCommunityIcons name="qrcode-edit" color={color} size={size} />
+			),
+		  }}
+		/>
+		
+	
 	  </Tab.Navigator>
 	);
 }
 
-export default function App() {
+const Stack = createStackNavigator();
+const HomeStack = () => {
+  return (
+    <Stack.Navigator initialRouteName='Contact Card'>
+      <Stack.Screen name="Contact Card" component={QRCodeGenerator} options={{ headerShown: false }}/>
+      <Stack.Screen name="QRCard" component={QRCodeDisplay} />
+    </Stack.Navigator>
+  )
+}
+
+function App() {
   return (
     <NavigationContainer>
-      <MyTabs />
+            <MyTabs/>
     </NavigationContainer>
+   
   );
 }
+
+export default App;
